de45722162942f23bdb5cc8be5caffe52034f5ae
Pitambar <pitambarzha@gmail.com>
2020-09-13 00:52:59 +0545

    Installed xunit and moq for test class library

---
+++ LibMS.Test/BookControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using System.Threading.Tasks;
+
+namespace LibMS.Test
+{
+    public class BookControllerTest
+    {
+        [Fact]
+        public async Task GetAll_NoBooks_NoReturns()
+        {
+            var bookServiceMock = new Mock<IBookService>();
+            var bookController = new BookController(bookService);
+            var response = bookController.Get();
+            bookServiceMock.Verify(x => x.GetCurrentBookAsync());
+        }
+    }
+}
